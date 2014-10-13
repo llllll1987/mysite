048864044944a49e22ac463940bcc09cc1810691
llllll1987 <ligefei870828@sina.com>
2014-10-13 16:26:48 +0800

    Add Bulk change owner to audit DB.

--- myblog/views.py
+++ myblog/views.py
@@ -2,7 +2,7 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response,HttpResponse
-from myblog.models import Blog,Zhiwei_CMC_Board,CMC_Board,SFPVendor,SFP,Record_log
+from myblog.models import Blog,Author,Zhiwei_CMC_Board,CMC_Board,SFPVendor,SFP,Record_log
 
 
 def blog_list(request):
@@ -45,4 +45,26 @@ def sfp_tracking(request):
     temp = sfps.exclude(sfp_owner_id = 1)
     sfpcount["in_crossteam"] = temp.count()
     return render_to_response("sfp_tracking.html", {"sfps": sfps, "sfpvendors": sfpvendors, "sfpcount":sfpcount,"records":records})
+
+
+def debug_cmd(request):
+    input_string = 'FNS183815VR SPC18330309 FNS183815U9 EAD2012243 SPC183301KQ SPC183301KP SPC183301KN SPC183301KM SPC183301KL FNS18340YEL'
+    sfp_sn_list = input_string.split()
+
+    sfp_all = SFP.objects.all()
+    owner_all = Author.objects.all()
+    newsfp_owner = owner_all.get(name = 'yizh')
+    change_count = 0
+
+    for temp_sfp_sn in sfp_sn_list:
+        temp_sfp = sfp_all.get(sfp_sn= temp_sfp_sn)
+        print temp_sfp
+        temp_sfp.owner = newsfp_owner
+        print temp_sfp.owner
+        temp_sfp.save()
+        temp_sfp.delete()
+        print temp_sfp
+        change_count = change_count + 1
+    html = "<html><body>temp_sfp is %s </body></html>" % temp_sfp
+    return HttpResponse(html)
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,5 +23,6 @@ urlpatterns = patterns('',
     url(r'^zhiwei_board_list/$',zhiwei_board_list,name = 'zhiwei_board_list'),
     url(r'^board_tracking/$',board_tracking,name = 'board_tracking'),
     url(r'^sfp_tracking/$',sfp_tracking,name = 'sfp_tracking'),
+    url(r'^debugcmd/$',debug_cmd,name = 'debug_cmd'),
 
 )
